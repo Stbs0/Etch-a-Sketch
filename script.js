c38f9e7a4204449877707f9787722c6844643a1a
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-12-03 12:21:01 +0300

    problem: cants fix the flexbasis

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// all the variables
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
-container.className = "container";
+// the excisting grid
 for (i = 0; i < 256; i++) {
   let grid = document.createElement("div");
   grid.addEventListener("mouseover", function () {
@@ -9,3 +11,23 @@ for (i = 0; i < 256; i++) {
   grid.className = "square";
   container.appendChild(grid);
 }
+
+btn.addEventListener("click", function () {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  answer = prompt("How many square per grid you want?");
+  for (i = 0; i < answer * answer; i++) {
+    let grid = document.createElement("div");
+    grid.addEventListener("mouseover", function () {
+      grid.style.backgroundColor = "black";
+      grid.style.flexBasis = " 100% / answer";
+    });
+    grid.className = "square";
+    let flex = 100 / answer;
+    grid.style.flexBasis = flex;
+
+    container.appendChild(grid);
+  }
+});
