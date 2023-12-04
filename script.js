e4fb822c72a01af8f8fe572c1c40051661361b8e
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-12-04 07:52:14 +0300

    finished the previous issue regarding the rows

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ btn.addEventListener("click", function () {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-
   answer = prompt("How many square per grid you want?");
-  for (i = 0; i < answer * answer; i++) {
-    let grid = document.createElement("div");
-    grid.addEventListener("mouseover", function () {
-      grid.style.backgroundColor = "black";
-      grid.style.flexBasis = " 100% / answer";
-    });
-    grid.className = "square";
-    let flex = 100 / answer;
-    grid.style.flexBasis = flex;
-
-    container.appendChild(grid);
+  answer = parseInt(answer);
+  if (typeof answer === "number" && answer <= 64) {
+    for (i = 0; i < answer * answer; i++) {
+      let grid = document.createElement("div");
+      grid.addEventListener("mouseover", function () {
+        grid.style.backgroundColor = "black";
+        grid.style.flexBasis = " 100% / answer";
+      });
+      grid.className = "square";
+      grid.style.flexBasis = `calc(100% / ${answer})`;
+      grid.style.height = `calc(100% / ${answer})`;
+      container.appendChild(grid);
+    }
+  } else {
+    alert("must be less than 64 and is a number");
   }
 });
